42109e7f91021beb387a6fc3ee34442ad2f3d38c
ufebri <ufebri@live.com>
2018-12-20 13:37:42 +0700

    Random Lucky Number App

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:learning_flutter/app_screens/first_screen.dart';
+
+void main() => runApp(new MyFlutterApp());
+
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "My Flutter App",
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("My First App Screen"),
+            ),
+            body: FirstScreen()));
+  }
+}
