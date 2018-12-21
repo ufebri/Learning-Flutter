09d02f78e9aa9cd699dccff50bf56122077392d3
ufebri <ufebri@live.com>
2018-12-21 16:00:09 +0700

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,46 @@ import 'package:learning_flutter/app_screens/home.dart';
 void main() {
   runApp(MaterialApp(
     title: "Exploring UI widgets",
-    home: Home(),
-  )
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Basic List View"),
+      ),
+      body: getListView(),
+    ),
+  ));
+}
+
+Widget getListView() {
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beatiful View !"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          debugPrint("Landscape tapped");
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_mac),
+        title: Text("Mac"),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone_android),
+        title: Text("Phone"),
+      ),
+      ListTile(
+        leading: Icon(Icons.mail),
+        title: Text("Mail"),
+      ),
+      Text("Yet another element in List"),
+      Container(
+        color: Colors.red,
+        height: 50.0,
+      )
+    ],
   );
+
+  return listView;
 }
